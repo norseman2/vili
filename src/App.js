484a58392f23c2f713f8137571a42e10891f82c8
Fred Ory <fred@systemike.com>
2020-08-27 21:13:17 +0000

    electricity smart meters grid

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
+//devexpress grid
+import Paper from '@material-ui/core/Paper';
+import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui';
 
 import Tsmk from './components/Tsmk';
 import SmartMeters from './components/electricity/SmartMeters';
+
+import { API, graphqlOperation } from 'aws-amplify'
+import { listSmartMeters as ListSmartMeters } from './graphql/queries'
 
 class App extends React.PureComponent {
 
@@ -16,16 +22,42 @@ class App extends React.PureComponent {
     super(props);
     this.handleLoadProfileChange = this.handleLoadProfileChange.bind(this)
     this.state = {
-      loadProfile: []
+      loadProfile: [],
+      smartMeterColumns : [
+        {name:'name',title:'Name'},
+        {name:'type',title:'Type'},
+        {name:'unit',title:'Unit'}
+      ],
+      smartMeterRows: []
     };
   }  
 
   async componentDidMount() {
     try {
+      this.fetchSmartMeters()
     } catch (err) {
       console.log('error', err)
     }
   }  
+
+  fetchSmartMeters = async() => {
+    try {
+      await API.graphql(graphqlOperation(ListSmartMeters)).then(
+        (result) => {
+          const smartMeters = result.data.listSmartMeters.items;
+          console.log('smartMeters',smartMeters)
+          this.setState({
+            smartMeterRows: smartMeters
+          })
+        }
+      ).catch( (err) => {
+        console.log(err)
+      })
+    } catch(err) {
+      console.log('err',err)
+    }
+  } 
+
 
   handleLoadProfileChange(loadProfile) {
     console.log('handleLoadProfileChange',loadProfile)
@@ -34,7 +66,7 @@ class App extends React.PureComponent {
 
   render() {
 
-    const loadProfile = this.state.loadProfile;
+    const {loadProfile,smartMeterColumns,smartMeterRows} = this.state;
 
     return (
       <Container className="p-3">
@@ -50,6 +82,15 @@ class App extends React.PureComponent {
             </Card.Header>
             <Accordion.Collapse eventKey="0">
               <Card.Body>
+                <Paper>
+                  <Grid
+                    rows={smartMeterRows}
+                    columns={smartMeterColumns}
+                  >
+                    <Table />
+                    <TableHeaderRow />
+                  </Grid>
+                </Paper>
                 <Tsmk loadProfile={loadProfile} onLoadProfileChange={this.handleLoadProfileChange} />
               </Card.Body>
             </Accordion.Collapse>
