c8f4812d2eafcfd1d7f848c73e1155ccd1a699c9
Fred Ory <fred@systemike.com>
2020-09-19 15:52:29 +0000

    spark job

--- spark/vili-spark.py
+++ spark/vili-spark.py
@@ -3,6 +3,6 @@ import boto3
 def getS3Object(source,target):
 	bucket = 'vili-bucket'
 	s3_client = boto3.client('s3')
-	s3.download_file(bucket, source, target)
+	s3_client.download_file(bucket, source, target)
 
 getS3Object('spark/vili-electricity-consumption.csv','/home/hadoop/data/in/')
