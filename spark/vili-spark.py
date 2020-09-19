76eb1ca72c0154d1bdc0dca1f569e9f2206a88b2
Fred Ory <fred@systemike.com>
2020-09-19 16:24:59 +0000

    spark job

--- spark/vili-spark.py
+++ spark/vili-spark.py
@@ -2,14 +2,33 @@ import boto3
 import shutil
 from datetime import datetime
 
-sourcefile='spark/vili-electricity-consumption.csv'
-targetfile='/home/hadoop/data/in/vili-electricity-consumption.csv'
-archivefile='/home/hadoop/data/archives/in/vili-electricity-consumption.csv' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '-UTC.csv'
+import pyspark.sql.types import *
 
+s3_source='spark/vili-electricity-consumption.csv'
+spark_in='/home/hadoop/data/in/vili-electricity-consumption.csv'
+archive_in='/home/hadoop/data/archives/in/vili-electricity-consumption-' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '-UTC.csv'
+spark_out='/home/hadoop/data/out/vili-electricity-consumption.csv'
+archive_out='/home/hadoop/data/archives/out/vili-electricity-consumption-' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '-UTC.csv'
+archive_s3='forecast/archives/vili-electricity-consumption-' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '-UTC.csv'
+s3_target='forecast/vili-electricity-consumption.csv'
+
+'''
 def getS3Object(source,target):
 	bucket = 'vili-bucket'
 	s3_client = boto3.client('s3')
 	s3_client.download_file(bucket, source, target)
 
-shutil.move(targetfile,archivefile)
-getS3Object(sourcefile,targetfile)
+shutil.move(spark_in,archive_in)
+getS3Object(s3_source,spark_in)
+'''
+
+schema = StructType([
+	StructField('item_id',      StringType(), True),
+    StructField('timestamp',    StringType(), True),
+	StructField('target_value', FloatType(), True)
+])
+
+df = spark.read.csv(spark_in, header=True, schema=schema)
+
+shutil.move(spark_out,archive_out)
+df.write.format("csv").save(spark_out)
